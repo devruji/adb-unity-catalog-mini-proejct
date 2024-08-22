6ff0b9e3c62b5803902ac178507dcc690cbc51fb
en.rujikorn@gmail.com <en.rujikorn@gmail.com>
2024-08-22 05:35:45 +0000

    initial commit

---
+++ 1.create-ext-locations.sql
@@ -0,0 +1,10 @@
+-- Databricks notebook source
+-- MAGIC %md Create the external locations required for this project
+-- MAGIC
+-- MAGIC 1. Bronze
+-- MAGIC 2. Silver
+-- MAGIC 3. Gold
+
+-- COMMAND ----------
+
+
