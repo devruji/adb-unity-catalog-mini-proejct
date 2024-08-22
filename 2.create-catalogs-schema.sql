c53d8edaa22ec31219882f678dbef0a1677d2660
en.rujikorn@gmail.com <en.rujikorn@gmail.com>
2024-08-22 06:08:57 +0000

    initial commit

---
+++ 2.create-catalogs-schema.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC
+-- MAGIC ### Create catalogs and Schemas required for the project
+-- MAGIC
+-- MAGIC 1. Catalog - Formula1dev (Without managed location)
+-- MAGIC 2. Schemas - bronze, silver, and gold. (with managed location)
+
+-- COMMAND ----------
+
+
